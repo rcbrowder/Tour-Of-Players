701b3d95d957d30b7fe70235abfa454cf0000b40
Chris Browder <rcbrowder@gmail.com>
2018-12-08 15:54:57 -0500

    Added PlayerService class, registered it as a provider, and injected it into a component.

--- tour-of-players/src/app/players/players.component.ts
+++ tour-of-players/src/app/players/players.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Player } from '../hero';
+import { Player } from '../player';
 import { PlayerService } from '../player.service';
 
 @Component({
@@ -9,12 +9,11 @@ import { PlayerService } from '../player.service';
 })
 export class PlayersComponent implements OnInit {
 
-    players: Player;
+    players: Player[];
     onSelect(player: Player): void {
         this.selectedPlayer = player;
     }
 
-    players = PLAYERS;
 
     getPlayers(): void {
         this.players = this.playerService.getPlayers();
@@ -22,7 +21,10 @@ export class PlayersComponent implements OnInit {
 
 
 
-  constructor(private PlayerService: PlayerService) { }
+
+  constructor(private playerService: PlayerService) {
+
+   }
 
   ngOnInit() {
       this.getPlayers();
