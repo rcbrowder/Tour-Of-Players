0ec5420c3982e9fe2dcf1279db79939203a77ccf
Chris Browder <rcbrowder@gmail.com>
2018-12-08 15:18:21 -0500

    Created separate PlayerDetailComponent. Used property binding to give the parent PlayersComponent control over the child PlayerDetailComponent.

--- tour-of-players/src/app/app.module.ts
+++ tour-of-players/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PlayersComponent } from './players/players.component';
 
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
+import { PlayerDetailComponent } from './player-detail/player-detail.component'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayersComponent
+    PlayersComponent,
+    PlayerDetailComponent
   ],
   imports: [
     BrowserModule,
